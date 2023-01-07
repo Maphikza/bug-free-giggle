3fa631905e23fa0b1fe7ed499948c30079131c22
Maphikza <stapisi@protonmail.com>
2023-01-07 14:20:31 +0200

    Adding Exception handlers to avoid runtime crashes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotInteractableException, ElementNotVisibleException
 import json
 import time
 
@@ -18,31 +19,32 @@ def get_cik():
     return dict(zip(securities_list, cik_list))
 
 
-if __name__ == "__main__":
+def fetch_outstanding_stock():
     company_cik_dict = get_cik()
-
     with open("stocks_and_cik.json", encoding='utf-8', mode='w') as output_file:
         json.dump(company_cik_dict, output_file)
-
-    data_urls = []
-    run_logs = []
 
     for i, key in enumerate(company_cik_dict):
         service = Service(path)
         driver = webdriver.Chrome(service=service)
 
-        url = f"https://www.sec.gov/edgar/browse/?CIK={company_cik_dict[key]}&owner=exclude"
+        url = f"https://www.sec.gov/edgar/browse/?CIK={int(company_cik_dict[key])}&owner=exclude"
 
         driver.get(url)
         time.sleep(3)
         section = driver.find_element(By.XPATH, '//*[@id="filingsStart"]/div[2]/div[3]/h5/a')
+        try:
+            ActionChains(driver=driver) \
+                .move_to_element(section) \
+                .pause(1) \
+                .click() \
+                .pause(1) \
+                .perform()
+        except (ElementNotInteractableException, ElementNotVisibleException):
+            with open("last_run_logs.txt", "a") as inner_log_file:
+                inner_log_file.write(f"This URL{url} failed.")
 
-        ActionChains(driver=driver) \
-            .move_to_element(section) \
-            .pause(1) \
-            .click() \
-            .pause(1) \
-            .perform()
+            continue
 
         filing = driver.find_element(By.XPATH, '//*[@id="selected-filings-annualOrQuarterly"]/ul/li[1]')
 
@@ -67,31 +69,27 @@ if __name__ == "__main__":
                 break
 
         new_url = driver.current_url
-        data_urls.append(new_url)
-        driver.quit()
+
         time.sleep(3)
-        run_logs.append(f"{key}: {company_cik_dict[key]} appended, successfully.")
+
         print(f"{key}")
         time.sleep(1)
 
-    run_logs.append(f"The length of the url list is: {len(data_urls)}")
-    time.sleep(1)
-    outstanding_shares_text_data = []
-
-    for link in data_urls:
-        service = Service(path)
-        driver = webdriver.Chrome(service=service)
-        form_url = link
-        driver.get(form_url)
+        time.sleep(1)
 
         filter_data = driver.find_element(By.XPATH, '//*[@id="nav-filter-data"]/span[2]')
+        try:
+            ActionChains(driver=driver) \
+                .move_to_element(filter_data) \
+                .pause(2) \
+                .click() \
+                .pause(1) \
+                .perform()
+        except (ElementNotInteractableException, ElementNotVisibleException):
+            with open("last_run_logs.txt", "a") as inner_log_file:
+                inner_log_file.write(f"This URL{new_url} failed.")
 
-        ActionChains(driver=driver) \
-            .move_to_element(filter_data) \
-            .pause(2) \
-            .click() \
-            .pause(1) \
-            .perform()
+            continue
 
         amounts_only = driver.find_element(By.XPATH, '//*[@id="nav-filter-dropdown"]/div[2]/input')
 
@@ -113,16 +111,19 @@ if __name__ == "__main__":
         time.sleep(1)
         amounts_data = driver.find_element(By.XPATH, '//*[@id="taxonomies-menu-list-pagination"]/div[3]')
 
-        outstanding_shares_text_data.append(f"('{link}','{amounts_data.text}')")
-        run_logs.append(f"({link}) - data was captured successfully.")
+        with open("outstanding_shares_text.txt", "a") as scraped_output:
+            scraped_output.write(f"({i})\n('{new_url}','{amounts_data.text}')\n")
+        time.sleep(1)
+        with open("last_run_logs.txt", "a") as inner_log_file:
+            inner_log_file.write(f"{i} {key}: {company_cik_dict[key]} appended, successfully.\n({new_url}) - data was "
+                                 f"captured successfully.\n")
+        time.sleep(1)
+
         driver.quit()
 
-    with open("outstanding_shares_text.txt", "w") as scraped_output:
-        content = "\n".join(outstanding_shares_text_data)
-        scraped_output.writelines(content)
 
-    with open("last_run_logs.txt", "w") as log_file:
-        file_content = "\n".join(run_logs)
-        log_file.writelines(file_content)
-
+if __name__ == "__main__":
+    fetch_outstanding_stock()
+    with open("last_run_logs.txt", "a") as log_file:
+        log_file.write("Success at last!")
     print(f"Success!")
